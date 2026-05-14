78dbe02399dbccf2798b2d49c785fb63ea74ce69
rakibulhasan044 <rhrakib044@gmail.com>
2026-05-15 04:05:55 +0600

    updated appointment create

--- src/app/modules/appointment/appointment.service.ts
+++ src/app/modules/appointment/appointment.service.ts
@@ -17,7 +17,7 @@ const createAppointment = async (user: IAuthUser, payload: any) => {
     },
   });
 
-  const doctorScheduleData = await prisma.doctorSchedules.findFirstOrThrow({
+  await prisma.doctorSchedules.findFirstOrThrow({
     where: {
       doctorId: doctorData.id,
       scheduleId: payload.scheduleId,
@@ -25,20 +25,35 @@ const createAppointment = async (user: IAuthUser, payload: any) => {
     },
   });
 
-  const videoCallingId = uuidv4();
+  const videoCallingId: string = uuidv4();
 
-  const result = await prisma.appointment.create({
-    data: {
-      patientId: patientData.id,
-      doctorId: doctorData.id,
-      scheduleId: payload.scheduleId,
-      videoCallingId,
-    },
-    include: {
-      patient: true,
-      doctor: true,
-      schedule: true,
-    },
+  const result = await prisma.$transaction(async (tx) => {
+    const appointmentData = await tx.appointment.create({
+      data: {
+        patientId: patientData.id,
+        doctorId: doctorData.id,
+        scheduleId: payload.scheduleId,
+        videoCallingId,
+      },
+      include: {
+        patient: true,
+        doctor: true,
+        schedule: true,
+      },
+    });
+
+    await tx.doctorSchedules.update({
+      where: {
+        doctorId_scheduleId: {
+          doctorId: doctorData.id,
+          scheduleId: payload.scheduleId,
+        },
+      },
+      data: {
+        isBooked: true,
+        appointmentId: appointmentData.id
+      },
+    });
   });
 
   return result;
